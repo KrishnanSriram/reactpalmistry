15d75f148a0c994bd6256a5e3b12faf84a68e386
Krishnan <krishnan.sriram@hotmail.com>
2024-09-02 14:42:10 -0400

    Updated footer

--- src/component/appfooter.js
+++ src/component/appfooter.js
@@ -5,7 +5,8 @@ export const AppFooter = () => {
     <footer className="bg-light text-center text-lg-start mt-5">
       <div className="container p-4">
         <div className="text-center p-3">
-          &copy; 2024 MyApp. All rights reserved.
+          &copy; 2024 EzPalmistry. All rights reserved. Unauthorized reproduction or distribution of this content, including but not limited to text, images, and code, is strictly prohibited. Website designed and developed by EzPalmistry. 
+          <p></p>
         </div>
       </div>
     </footer>

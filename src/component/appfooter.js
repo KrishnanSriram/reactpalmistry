0316521a61cac895f4647d0a59bb67ef6cfa637c
Krishnan <krishnan.sriram@hotmail.com>
2024-09-02 15:25:52 -0400

    Fixed spell mistakes

--- src/component/appfooter.js
+++ src/component/appfooter.js
@@ -5,7 +5,7 @@ export const AppFooter = () => {
     <footer className="bg-light text-center text-lg-start mt-5">
       <div className="container p-4">
         <div className="text-center p-3">
-          &copy; 2024 EzPalmistry. All rights reserved. Unauthorized reproduction or distribution of this content, including but not limited to text, images, and code, is strictly prohibited. Website designed and developed by EzPalmistry. 
+          &copy; 2024 EsyPalmistry. All rights reserved. Unauthorized reproduction or distribution of this content, including but not limited to text, images, and code, is strictly prohibited. Website designed and developed by EsyPalmistry. 
           <p></p>
         </div>
       </div>
